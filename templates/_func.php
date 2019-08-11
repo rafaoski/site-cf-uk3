f7f1bf5ed5e33c74982e6f89dc4c7faf9438e495
Rafał Olszewicz <rafalolszewicz@gmail.com>
2019-08-11 17:49:50 +0200

    upd icon ratio

--- templates/_func.php
+++ templates/_func.php
@@ -726,7 +726,7 @@ function socialProfiles($items)
 		$out .= ukIcon($item->name,
 		[
 			'class' => 'transition-bounce-s margin-xxxs color-warning',
-			'ratio' => 1.5
+			'ratio' => '1.7'
 		]) . "</a>";
 	}
 
