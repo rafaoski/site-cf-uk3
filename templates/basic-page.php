8a35d39d3cc5fe89401b8d737bae38fc9d995ab0
Rafał Olszewicz <rafalolszewicz@gmail.com>
2019-08-11 18:33:34 +0200

    upd intro

--- templates/basic-page.php
+++ templates/basic-page.php
@@ -1,1 +1,24 @@
 <?php namespace ProcessWire;
+
+if (page()->hasChildren): ?>
+<div id='main-content' data-pw-append>
+
+<h3 class='uk-h2'><?= setting('more-topic') ?>:</h3>
+
+	<div class="body__content uk-grid-small uk-child-width-expand@m" data-uk-grid>
+		<?php foreach(page()->children as $item): ?>
+		<div>
+			<div class='uk-card uk-card-default uk-card-body'>
+				<h3 class='text-md'><?= $item->title ?></h3>
+				<p class='text-sm'><?= $item->intro ?></p>
+				<a class='uk-button uk-button-primary uk-margin-small' href="<?= $item->url ?>">
+					<?=  setting('read-more') . ' ' . ukIcon('chevrons-right') ?>
+				</a>
+			</div>
+		</div>
+
+		<?php endforeach; ?>
+	</div>
+
+</div>
+<?php endif;
